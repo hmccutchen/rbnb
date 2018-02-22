5c2e6a054dcc497ee24487f8b5e70f53458c6d07
hassan mccutchen <has_bum@yahoo.com>
2018-02-22 11:52:17 +0800

    dog model and validations

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,9 @@
 class Dog < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :age, presence: true
+  validates :breed, presence: true
+
+
 end
